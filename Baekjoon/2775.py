c4f7fc19bbebc101874f0ffe38ead6584e9556d6
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-06-30 11:46:23 +0900

    Update 2775.py

--- Baekjoon/2775.py
+++ Baekjoon/2775.py
@@ -1,2 +1,11 @@
-test
-23456789101112
+import sys
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    k = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
+    f0 = [ i for i in range(1,n+1)]
+    for i in range(k):
+        for j in range(1,n):
+            f0[j] += f0[j-1]
+    print(f0[-1])
