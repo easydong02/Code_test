84557cc67050b7381e2a4e5e4491b0fd81581616
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-07-23 22:26:08 +0900

    Create 2839.java

---
+++ Baekjoon/2839.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2839 {
+	static int N;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		while(N>4) {
+			
+		}
+		
+	}
+
+}
