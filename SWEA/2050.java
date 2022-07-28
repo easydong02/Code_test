ad8fc4305e370472a5e5a7ebd7fab5f2a55fe795
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-07-28 20:32:33 +0900

    Create 2050.java

---
+++ SWEA/2050.java
@@ -0,0 +1,23 @@
+package swea.d1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Swea2050 {
+	private static String str;
+	private static char[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		str= br.readLine();
+		
+		arr= str.toCharArray();
+		
+		for(char c: arr) {
+			System.out.print(c-64 +" ");
+		}
+	}
+
+}
