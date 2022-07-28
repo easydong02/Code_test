ea3efff93e1e610a44c9de6a8f4837d9b15f6665
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-07-28 23:25:35 +0900

    Update 2839.java

--- Baekjoon/2839.java
+++ Baekjoon/2839.java
@@ -10,10 +10,22 @@ public class Bj2839 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
+		int cnt =N/3;
 		
-		while(N>4) {
-			
+		if(N%5 ==0) {
+			System.out.println(N/5);
+			System.exit(0);
 		}
+		
+		for (int i = 0; i <= cnt; i++) {
+			for(int j =0; j<=cnt; j++) {
+				if((3*i + 5*j)==N) {
+					System.out.println(i +j);
+					System.exit(0);
+				}
+			}
+		}
+		System.out.println(-1);
 		
 	}
 
