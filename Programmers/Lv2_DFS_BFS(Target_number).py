5dc6498eceb08c12450c21add1f0b653ee7b835f
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-10-12 23:34:52 +0900

    Update Lv2_DFS_BFS(Target_number).py

--- Programmers/Lv2_DFS_BFS(Target_number).py
+++ Programmers/Lv2_DFS_BFS(Target_number).py
@@ -5,6 +5,7 @@ def solution(numbers, target):
     numbers_= [2*i for i in numbers]
     make = sum(numbers) - target
     for i in range(1,len(numbers_)+1):
+        
         result =  [sum(e) for e in  list(combinations(numbers_,i))]
         for j in result:
             if j ==make:
