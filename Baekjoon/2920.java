aa84fdbcaaee4ee15d8aed7292223f3fa0d27cb5
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-12-29 21:03:29 +0900

    Create 2920.java

---
+++ Baekjoon/2920.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj2920 {
+
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		arr= new int[8];
+		for (int i = 0; i < 8; i++) {
+			arr[i]= Integer.parseInt(st.nextToken());
+		}//end of reading
+		
+		boolean chk = true;
+		int dist= arr[1]-arr[0];
+		
+		for (int i = 0; i < 7; i++) {
+			if(arr[i+1]-arr[i] != dist) {
+				chk =false;
+				break;
+			}
+		}
+		
+		if(!chk) {
+			System.out.println("mixed");
+		}else {
+			if(dist==1)System.out.println("ascending");
+			else if(dist==-1)System.out.println("descending");
+		}
+	}
+
+}
