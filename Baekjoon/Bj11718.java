cfdd8de244dd68bcf868699cc91f1828334eb951
엉덩희 <82931413+easydong02@users.noreply.github.com>
2023-01-08 16:27:34 +0900

    Create Bj11718.java

---
+++ Baekjoon/Bj11718.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bj11718 {
+
+	public static void main(String[] args) throws IOException {
+//		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+//		
+//		StringBuilder sb= new StringBuilder();
+//		
+//		System.out.println(Arrays.toString( br.readLine().toCharArray()));
+		
+		
+		Scanner sc= new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+
+}
