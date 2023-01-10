b4ed209912eb2c108d355638dc34c51ad74563da
easydong02 <easydong02@gmail.com>
2023-01-10 21:48:12 +0900

    [2023-01-10] BOJ_2743 단어길이재기

---
+++ src/Baekjoon/Bj2743.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Bj2743 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String result = sc.nextLine();
+		System.out.println(result.length());
+	}
+
+}
