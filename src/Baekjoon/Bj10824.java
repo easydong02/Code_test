c804b96c4f7d3ecf7e7e9a062a0207540dadb212
easydong02 <easydong02@gmail.com>
2023-02-16 23:58:29 +0900

    [2023-02-16] BOJ 10824 네수

---
+++ src/Baekjoon/Bj10824.java
@@ -0,0 +1,59 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Bj10824 {
+	
+	static StringBuilder sb;
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		sb= new StringBuilder();
+		String[] input = sc.nextLine().split(" ");
+		
+		String a = input[0]+input[1];
+		String b= input[2]+input[3];
+		
+		Boolean more10 = false;
+		
+		int maxsize = Math.max(a.length(), b.length());
+		int minsize = Math.min(a.length(), b.length());
+		String big = a.length()> b.length()? a:b;
+		
+		if(maxsize==minsize) {
+			go(a,b,maxsize,more10);
+		}else {
+			go(a,b,minsize,more10);
+			
+			for (int i = maxsize-minsize-1; i >=0; i--) {
+				if(more10) {
+					sb.insert(0,Integer.parseInt(big.substring(i,i+1))+1 );
+					more10= false;
+				}else {
+					sb.insert(0,big.substring(i,i+1));
+				}
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+	private static void go(String a, String b, int size, Boolean more10) {
+		int sizeA= a.length();
+		int sizeB= b.length();
+		
+		while(size>0) {
+			int numa = Integer.parseInt(a.substring(--sizeA,sizeA+1));
+			int numb = Integer.parseInt(b.substring(--sizeB,sizeB+1));
+			int plus1 = more10? 1:0;
+			if(numa+numb+plus1>9) {
+				more10=true;
+				sb.insert(0,(numa+numb+plus1-10));
+			}else {
+				more10=false;
+				sb.insert(0,(numa+numb+plus1));
+			}
+			size--;
+		}
+	}
+
+}
