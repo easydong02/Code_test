3322d2c6dd6311e4a54c10b7c7a00c9210c662f6
easydong02 <easydong02@gmail.com>
2023-03-06 21:10:18 +0900

    [2023-03-06] BOJ 4999

---
+++ src/Baekjoon/Bj4999.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj4999 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		
+		if(str1.length()>=str2.length()) {
+			System.out.println("go");
+		}else {
+			System.out.println("no");
+		}
+	}
+
+}
