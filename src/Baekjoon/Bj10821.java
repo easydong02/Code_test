167fbecde48c09cf91738620d6e54baedf6638a7
easydong02 <easydong02@gmail.com>
2023-04-12 23:03:21 +0900

    [2023-04-12] BOJ 10821

---
+++ src/Baekjoon/Bj10821.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj10821 {
+	
+	static int result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(",");
+		
+		System.out.println(arr.length);
+	}
+
+}
