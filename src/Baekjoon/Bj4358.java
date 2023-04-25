3cc1a74cbcf3197e5f93bd556b82867a74cde360
easydong02 <easydong02@gmail.com>
2023-04-26 00:08:14 +0900

    [2023-04-26] BOJ 4358

--- src/Baekjoon/Bj4358.java
+++ src/Baekjoon/Bj4358.java
@@ -3,28 +3,42 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Bj4358 {
 	static Map<String, Integer> map;
+	static ArrayList<String> names;
 	static int num;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		String name;
 		map= new HashMap<String, Integer>();
-		while((name = br.readLine()) !=null) {
+		names = new ArrayList<>();
+		while(true) {
+			name = br.readLine();
+			if(name==null || name.length()==0)break;
+			
 			if(map.getOrDefault(name, -1)!=-1) {
-				map.replace(name,map.get(name));
+				map.replace(name,map.get(name)+1);
 			}else {
 				map.put(name, 1);
+				names.add(name);
 			}
 			num++;
 		}
 		
 		
 		StringBuilder result = new StringBuilder();
-		
+		Collections.sort(names);
+		for (int i = 0; i < names.size(); i++) {
+			String key= names.get(i);
+			String r = String.format("%.4f", (double)map.get(key)/num*100);
+			result.append(key+" "+r+"\n");
+		}
+		System.out.println(result.toString());
 	}
 
 }
