39c50f0e63d69996299ed3fea9fa0147959d63f7
easydong02 <easydong02@gmail.com>
2023-05-03 00:02:37 +0900

    [2023-05-02] update BOJ 15904

--- src/Baekjoon/Bj15904.java
+++ src/Baekjoon/Bj15904.java
@@ -4,22 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 public class Bj15904 {
 	private static final String answer = "I love UCPC";
 	private static final String fail = "I hate UCPC";
+	private static final String[] answers = {"U","C","P","C"};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		String[] arr=br.readLine().split(" ");
+		String str =br.readLine();
 		
-		Map<String, Integer> map =new HashMap<String, Integer>();
+		Queue<String> queue = new LinkedList<String>();
 		
-		for (int i = 0; i < arr.length; i++) {
-			String tmp = arr[i].substring(0,1);
-			map.put(tmp, 1);
+//		for (int i = 0; i < arr.length; i++) {
+//			String tmp = arr[i].substring(0,1);
+//			queue.add(tmp);
+//		}
+//		
+		int cnt =0;
+//		while(!queue.isEmpty()) {
+//			String tmp = queue.poll();
+//			if(tmp.equals(answers[cnt])) {
+//				cnt++;
+//				if(cnt==4)break;
+//			}
+//		}
+		
+		for (int i = 0; i < str.length(); i++) {
+			if(str.substring(i,i+1).equals(answers[cnt])) {
+				cnt++;
+				if(cnt ==4)break;
+			}
 		}
-		
+		if(cnt==4) {
+			System.out.println(answer);
+		}else {
+			System.out.println(fail);
+		}
 	}
 
 }
