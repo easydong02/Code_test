4e1008cbd0da17b6079c83edabc0209ff6c6a0f8
easydong02 <easydong02@gmail.com>
2023-05-15 19:38:40 +0900

    [2023-05-15] BOJ 1436 update

--- src/Baekjoon/Bj1436.java
+++ src/Baekjoon/Bj1436.java
@@ -10,32 +10,15 @@ public class Bj1436 {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		long num = 1;
+		long num = 0;
 		while(n >0) {
 			num++;
-			int chk = go(num);
-			if(chk==-1) {
+			if(String.valueOf(num).contains("666")) {
 				n--;
-			}else if(chk != -100) {
-				num - chk
 			}
 		}
 		System.out.println(num);
 	}
 
-	private static int go(long num) {
-		String a = String.valueOf(num);
-		for (int i = 0; i < a.length()-2 ; i++) {
-			if(a.substring(i,i+3).equals("666")) {
-				if(i == a.length()-3) {
-					return -1;
-				}else {
-					String tmp = a.substring(i+3);
-					return Integer.parseInt(tmp);
-				}
-			}
-		}
-		return -100;
-	}
 
 }
