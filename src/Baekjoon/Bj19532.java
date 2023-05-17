478d3f74359fdeca2c0fae3ef72ddbfa1562254f
easydong02 <easydong02@gmail.com>
2023-05-17 23:16:43 +0900

    [2023-05-17] BOJ 19532

--- src/Baekjoon/Bj19532.java
+++ src/Baekjoon/Bj19532.java
@@ -1,37 +1,26 @@
 package Baekjoon;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Bj19532 {
-
+	static int a,b,c,e,d,f;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] arr = br.readLine().split(" ");
-		
-		int a = Integer.parseInt(arr[0]);
-		int b = Integer.parseInt(arr[1]);
-		int c = Integer.parseInt(arr[2]);
-		int d = Integer.parseInt(arr[3]);
-		int e = Integer.parseInt(arr[4]);
-		int f = Integer.parseInt(arr[5]);
-		int tmp =a;
-		a *=d;
-		b *=d;
-		c *=d;
-		
-		d *=tmp;
-		e *=tmp;
-		f *=tmp;
-		int y =0;
-		int x =0;
-		y = (c-f)/(b-e);
-		
-		x = (c-b*y)/a;
-		
-		System.out.println(x+" "+y);
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		d=sc.nextInt();
+		e=sc.nextInt();
+		f=sc.nextInt();
+		for (int i = -999; i < 1000; i++) {
+			for (int j = -999; j < 1000; j++) {
+				if(a*i+b*j==c && d*i+e*j==f) {
+					System.out.println(i+" "+j);
+					System.exit(0);
+				}
+			}
+		}
 	}
 
 }
