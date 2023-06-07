4a16f53ed451b2282bd5b2e7c66de0bd5a947f0a
easydong02 <easydong02@gmail.com>
2023-06-07 22:52:37 +0900

    [2023-06-07] BOJ 7568

---
+++ src/Baekjoon/Bj7568.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj7568 {
+
+	public static void main(String[] args) {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
