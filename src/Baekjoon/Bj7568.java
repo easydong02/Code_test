4d5dc3f80abc5b0a5d4f61f1c22c9085ea461391
easydong02 <easydong02@gmail.com>
2023-06-10 17:17:23 +0900

    [2023-06-10] BOJ 7568

--- src/Baekjoon/Bj7568.java
+++ src/Baekjoon/Bj7568.java
@@ -1,12 +1,45 @@
 package Baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Bj7568 {
+	public static class Student implements Comparable<Student>{
+		int height;
+		int weight;
+		
+		public Student(int weight, int height) {
+			this.height = height;
+			this.weight = weight;
+		}
 
-	public static void main(String[] args) {
+		@Override
+		public int compareTo(Student o) {
+			if(this.weight > o.weight && this.height > o.height) {
+				return 1;
+			}else if (this.weight < o.weight && this.height < o.height) {
+				return -1;
+			}else {
+				return 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Student> que =new PriorityQueue<>();
+		
+		
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			que.add(new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		
+		
 	}
 
 }
