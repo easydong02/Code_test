a8a854ff70c9d933b9825106629ba03a84fafcbf
easydong02 <easydong02@gmail.com>
2024-04-05 00:00:21 +0900

    [2024-04-05] 알고리즘 해동 - bj22864

---
+++ src/Baekjoon/Bj22864.java
@@ -0,0 +1,43 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj22864 {
+    static int A;
+    static int B;
+    static int C;
+    static int M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int P = 0;
+        int result = 0;
+        int hour = 0;
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 종료 조건
+        while(hour < 24){
+            //일 가능?
+            if(P + A <= M){
+                //1시간 일했음
+                P += A;
+                result += B;
+            }else{
+                //1시간 쉬었음
+                P -= C;
+                P = Math.max(P, 0);
+            }
+
+            hour +=1;
+        }
+
+        System.out.println(result);
+    }
+}
