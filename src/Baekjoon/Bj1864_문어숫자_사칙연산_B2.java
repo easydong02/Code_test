41d20009ff013b0459ee1b8ce1db40003cb661c5
easydong02 <easydong02@gmail.com>
2024-04-07 22:10:46 +0900

    [2024-04-07] 알고리즘 해동 - bj1864

---
+++ src/Baekjoon/Bj1864_문어숫자_사칙연산_B2.java
@@ -0,0 +1,40 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj1864_문어숫자_사칙연산_B2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        StringBuilder result = new StringBuilder();
+        while (!(str = br.readLine()).equals("#")){
+            int resultNum = 0;
+            char[] charArray = str.toCharArray();
+            for(int i = 0; i < charArray.length; i++){
+                int num = makeNum(charArray[i]);
+                resultNum += num*(int) Math.pow(8, charArray.length-1-i);
+            }
+            result.append(resultNum+"\n");
+        }
+        System.out.println(result.toString());
+    }
+
+    private static int makeNum(char c) {
+        switch (c){
+            case '-': return 0;
+            case '\\': return 1;
+            case '(': return 2;
+            case '@': return 3;
+            case '?': return 4;
+            case '>': return 5;
+            case '&': return 6;
+            case '%': return 7;
+            case '/': return -1;
+        }
+
+        return -2;
+    }
+}
