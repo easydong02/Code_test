512366c94ce466237394a4e097ac867ac7ec25a0
easydong02 <easydong02@gmail.com>
2024-04-07 21:51:31 +0900

    [2024-04-07] 알고리즘 해동 - bj21312

---
+++ src/Baekjoon/Bj21312.java
@@ -0,0 +1,41 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Bj21312 {
+
+    static int A;
+    static int B;
+    static int C;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        List<Integer> nums = new ArrayList<>();
+
+        if(A%2!=0) nums.add(A);
+        if(B%2!=0) nums.add(B);
+        if(C%2!=0) nums.add(C);
+
+        int result = 1;
+
+        if(nums.size() == 0){
+            result = A*B*C;
+        } else{
+            for (Integer num : nums) {
+                result *=num;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
